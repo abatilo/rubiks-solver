262aab0b9f4eb421daefb22ace6584870d938454
Aaron <AaronBatilo@gmail.com>
2017-12-25 19:34:43 -0700

    Implement U_PRIME

--- solver/src/main/java/RubiksSolverApplication.java
+++ solver/src/main/java/RubiksSolverApplication.java
@@ -1,11 +1,8 @@
 public class RubiksSolverApplication {
   public static void main(String[] args) {
     printCube(cube);
-    System.out.println();
     printCube(U(cube));
-    printCube(U(U(cube)));
-    printCube(U(U(U(cube))));
-    printCube(U(U(U(U(cube)))));
+    printCube(U_PRIME(U(cube)));
   }
 
   private static char[] cube = new char[] {
@@ -39,6 +36,7 @@ public class RubiksSolverApplication {
       }
       System.out.println();
     }
+    System.out.println();
   }
 
   private static char[] U(char[] cube) {
@@ -82,6 +80,39 @@ public class RubiksSolverApplication {
 
   private static char[] U_PRIME(char[] cube) {
     char[] copy = copyToNew(cube);
+    //    27 28 29
+    // 38 39 40 41 42
+    // 50 51 52 53 54
+    // 62 63 64 65 66
+    //    75 76 77
+
+    char outerEdgesTemp1 = copy[27];
+    char outerEdgesTemp2 = copy[28];
+    char outerEdgesTemp3 = copy[29];
+    copy[27] = copy[42];
+    copy[28] = copy[54];
+    copy[29] = copy[66];
+    copy[42] = copy[77];
+    copy[54] = copy[76];
+    copy[66] = copy[75];
+    copy[75] = copy[38];
+    copy[76] = copy[50];
+    copy[77] = copy[62];
+    copy[38] = outerEdgesTemp3;
+    copy[50] = outerEdgesTemp2;
+    copy[62] = outerEdgesTemp1;
+
+    char innerCornerTemp = copy[41];
+    copy[41] = copy[65];
+    copy[65] = copy[63];
+    copy[63] = copy[39];
+    copy[39] = innerCornerTemp;
+
+    char innerEdgeTemp = copy[40];
+    copy[40] = copy[53];
+    copy[53] = copy[64];
+    copy[64] = copy[51];
+    copy[51] = innerEdgeTemp;
     return copy;
   }
 
